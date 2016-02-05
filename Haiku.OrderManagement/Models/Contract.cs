7b811624a1b66e7385e91839bb419b63490fd016
Giles Davies <giles.j.davies@hotmail.com>
2016-02-05 10:33:21 +0000

    Add OrderManagement

---
+++ Haiku.OrderManagement/Models/Contract.cs
@@ -0,0 +1,38 @@
+namespace Models
+{
+    using System;
+    // a comment
+
+    public class Contract
+    {
+        private uint monthlyQuantity;
+
+        public bool IsCreditChecked {
+            get;
+            set;
+        }
+
+        public Location Location {
+            get;
+            set;
+        }
+
+        public uint MonthlyQuantity
+        {
+            get
+            {
+                return this.monthlyQuantity;
+            }
+
+            set
+            {
+                if (value == 0)
+                {
+                    throw new InvalidOrderException();
+                }
+
+                this.monthlyQuantity = value;
+            }
+        }
+    }
+}
---
+++ Haiku.OrderManagement/OrderCalculators/OrderCalculator.cs
@@ -0,0 +1,26 @@
+using System;
+using Models;
+
+namespace Contracts
+{
+
+    public class OrderCalculator
+    {
+
+        public uint CalculateOrderCosts(Customer customer, IOrderManagement contract)
+        {
+            //if (customer == null || contract == null) return default(uint);
+
+            var orderCosts = contract.ReOrder(
+                customer.Property.IsCreditChecked,
+                customer.Property.Location,
+                customer.AnnualBasicOrder,
+                customer.Property.MonthlyQuantity,
+                customer.AnnualOrderDiscountAllowance);
+
+            return orderCosts;
+        }
+
+    }
+
+}
